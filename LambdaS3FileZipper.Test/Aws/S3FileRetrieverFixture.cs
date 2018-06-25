12dbb9a4eac0c1acd0b4bf3ba026a9932501b1e9
Dave Rostocil <davidr@litmus.com>
2018-06-25 16:37:26 -0400

    test recieved

--- LambdaS3FileZipper.Test/Aws/S3FileRetrieverFixture.cs
+++ LambdaS3FileZipper.Test/Aws/S3FileRetrieverFixture.cs
@@ -12,24 +12,24 @@ namespace LambdaS3FileZipper.Test.Aws
 		private S3FileRetriever fileRetriever;
 
 		private IAwsS3Client s3Client;
+		private string testBucket = "test-bucket";
+		private string testResource = "test-resource";
 
 		[SetUp]
 		public void SetUp()
 		{
 			s3Client = Substitute.For<IAwsS3Client>();
-
+			s3Client.List(testBucket, testResource, Arg.Any<CancellationToken>()).Returns(new [] { "file1", "file2" });
 			fileRetriever = new S3FileRetriever(s3Client);
 		}
 
 		[Test]
 		public async Task List_ShouldReturnObjectPaths()
 		{
-			s3Client.List(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<CancellationToken>())
-				.Returns(new [] { "file1", "file2" });
-
-			var list = fileRetriever.Retrieve("bucket", "resource", CancellationToken.None);
+			var list = fileRetriever.Retrieve(testBucket, testResource, CancellationToken.None);
 
 			Assert.IsNotNull(list);
+			await s3Client.Received().List(testBucket, testResource, Arg.Any<CancellationToken>());
 		}
 	}
 }
