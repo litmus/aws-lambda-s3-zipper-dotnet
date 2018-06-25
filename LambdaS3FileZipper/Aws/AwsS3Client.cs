aa7bee43f8383f1e3a97921503f86e7252a57f88
Dave Rostocil <davidr@litmus.com>
2018-06-25 14:47:24 -0400

    initial AwsS3Client

---
+++ LambdaS3FileZipper/Aws/AwsS3Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace LambdaS3FileZipper
+{
+    public class AwsS3Client : IAwsS3Client
+    {
+	    private readonly IAmazonS3 client;
+
+		public RegionEndpoint RegionEndpoint { get; }
+
+	    public AwsS3Client(RegionEndpoint regionEndpoint)
+	    {
+		    RegionEndpoint = regionEndpoint;
+		    client = new AmazonS3Client(regionEndpoint);
+	    }
+
+	    public async Task<IEnumerable<string>> List(string bucketName, string resource, CancellationToken cancellationToken)
+	    {
+		    var objects = new List<string>();
+
+		    var request = new ListObjectsV2Request
+		    {
+				BucketName = bucketName
+		    };
+
+		    ListObjectsV2Response response;
+		    do
+		    {
+			    response = await client.ListObjectsV2Async(request, cancellationToken);
+			    objects.AddRange(response.S3Objects.Select(obj => obj.Key));
+		    } while (response.IsTruncated);
+			    
+			return objects;
+	    }
+
+	    public Task Upload(string bucketName, string resourceName, string destinationPath, CancellationToken cancellationToken)
+	    {
+		    throw new NotImplementedException();
+	    }
+    }
+}
