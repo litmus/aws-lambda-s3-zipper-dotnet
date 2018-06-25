330d59ee4189c35714c0ebe0777e83a4dbdacbe3
carlosneninger <carlos@litmus.com>
2018-06-25 14:51:18 -0400

    Adds namespaces to Models

--- LambdaS3FileZipper/Models/Request.cs
+++ LambdaS3FileZipper/Models/Request.cs
@@ -1,4 +1,14 @@
-public class Request
+namespace LambdaS3FileZipper.Models
 {
-    
+	public class Request
+	{
+		public Request(string bucketName, string resourceName)
+		{
+			BucketName = bucketName;
+			ResourceName = resourceName;
+		}
+
+		public string BucketName { get; }
+		public string ResourceName { get; }
+	}
 }
