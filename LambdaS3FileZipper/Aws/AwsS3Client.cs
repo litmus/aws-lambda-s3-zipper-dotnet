27231b166f601bfa0e9c8af3db8c35f16524ebc1
Dave Rostocil <davidr@litmus.com>
2018-06-25 15:29:05 -0400

    fix spacing

--- LambdaS3FileZipper/Aws/AwsS3Client.cs
+++ LambdaS3FileZipper/Aws/AwsS3Client.cs
@@ -9,40 +9,42 @@ using Amazon.S3.Model;
 
 namespace LambdaS3FileZipper.Aws
 {
-    public class AwsS3Client : IAwsS3Client
-    {
-	    private readonly IAmazonS3 client;
+	public class AwsS3Client : IAwsS3Client
+	{
+		private readonly IAmazonS3 client;
 
 		public RegionEndpoint RegionEndpoint { get; }
 
-	    public AwsS3Client(RegionEndpoint regionEndpoint)
-	    {
-		    RegionEndpoint = regionEndpoint;
-		    client = new AmazonS3Client(regionEndpoint);
-	    }
+		public AwsS3Client(RegionEndpoint regionEndpoint)
+		{
+			RegionEndpoint = regionEndpoint;
+			client = new AmazonS3Client(regionEndpoint);
+		}
 
-	    public async Task<IEnumerable<string>> List(string bucketName, string resource, CancellationToken cancellationToken)
-	    {
-		    var objects = new List<string>();
+		public async Task<IEnumerable<string>> List(string bucketName, string resource,
+			CancellationToken cancellationToken)
+		{
+			var objects = new List<string>();
 
-		    var request = new ListObjectsV2Request
-		    {
+			var request = new ListObjectsV2Request
+			{
 				BucketName = bucketName
-		    };
+			};
 
-		    ListObjectsV2Response response;
-		    do
-		    {
-			    response = await client.ListObjectsV2Async(request, cancellationToken);
-			    objects.AddRange(response.S3Objects.Select(obj => obj.Key));
-		    } while (response.IsTruncated);
-			    
+			ListObjectsV2Response response;
+			do
+			{
+				response = await client.ListObjectsV2Async(request, cancellationToken);
+				objects.AddRange(response.S3Objects.Select(obj => obj.Key));
+			} while (response.IsTruncated);
+
 			return objects;
-	    }
+		}
 
-	    public Task Upload(string bucketName, string resourceName, string localFile, CancellationToken cancellationToken)
-	    {
-		    throw new NotImplementedException();
-	    }
-    }
+		public Task Upload(string bucketName, string resourceName, string localFile,
+			CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
