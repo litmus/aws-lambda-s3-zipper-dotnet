32803a16e81127d6326fca7675c364e0091a745a
Dave Rostocil <davidr@litmus.com>
2018-06-25 16:11:57 -0400

    implement s3 file retriever

---
+++ LambdaS3FileZipper/Aws/S3FileRetriever.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using LambdaS3FileZipper.Interfaces;
+
+namespace LambdaS3FileZipper.Aws
+{
+	public class S3FileRetriever : IFileRetriever
+    {
+	    private readonly IAwsS3Client s3Client;
+
+	    public S3FileRetriever(IAwsS3Client s3Client)
+	    {
+		    this.s3Client = s3Client;
+	    }
+
+	    public async Task<IEnumerable<string>> Retrieve(string bucket, string resource)
+	    {
+		    return await s3Client.List(bucket, resource, CancellationToken.None);
+	    }
+    }
+}
