b21a65da46aedc1d3c67b88119a058c625e59de2
Dave Rostocil <davidr@litmus.com>
2018-06-25 13:59:01 -0400

    fix compile error

--- LambdaS3FileZipper/Interfaces/IFileUploader.cs
+++ LambdaS3FileZipper/Interfaces/IFileUploader.cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
+
 public interface IFileUploader
 {
-    Task Upload(string bucketName, string resourceName, string compressedFileName)
+	Task Upload(string bucketName, string resourceName, string compressedFileName);
 }
