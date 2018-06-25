84217f6cf3736c595a9e64c9876c368bf25881b2
carlosneninger <carlos@litmus.com>
2018-06-25 14:35:24 -0400

    Creates ZipperHandler as the main lambda handler, adds defined dependencies

---
+++ LambdaS3FileZipper/ZipperHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LambdaS3FileZipper
+{
+    public class ZipperHandler : IHandler
+    {
+	    private readonly IFileRetriever fileRetriever;
+	    private readonly IFileZipper fileZipper;
+	    private readonly IFileUploader fileUploader;
+
+		public ZipperHandler(IFileRetriever fileRetriever, IFileZipper fileZipper, IFileUploader fileUploader)
+		{
+			this.fileRetriever = fileRetriever;
+			this.fileZipper = fileZipper;
+			this.fileUploader = fileUploader;
+		}
+
+	    public Response Handle(Request request)
+	    {
+		    throw new NotImplementedException();
+	    }
+    }
+}
