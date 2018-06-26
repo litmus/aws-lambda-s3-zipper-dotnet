47a949bc1c13872f9e59ff47d66b3f5dab5a3957
Dave Rostocil <davidr@litmus.com>
2018-06-26 16:18:19 -0400

    zip integration test

---
+++ LambdaS3FileZipper.IntegrationTests/FileZipperFixture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using LambdaS3FileZipper.IntegrationTests.Logging;
+using NUnit.Framework;
+
+namespace LambdaS3FileZipper.IntegrationTests
+{
+	[TestFixture]
+	public class FileZipperFixture
+	{
+		private readonly FileZipper fileZipper;
+
+		public FileZipperFixture()
+		{
+			fileZipper = new FileZipper();
+		}
+
+		[Test]
+		public async Task Compress_ShouldZipFolder()
+		{
+			var tempDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+			Directory.CreateDirectory(tempDirectory);
+
+			var tempFile = Path.Combine(tempDirectory, "compress.txt");
+			await File.WriteAllTextAsync(tempFile, "Compress test.");
+
+			try
+			{
+				var zipFile = await fileZipper.Compress(tempDirectory);
+
+				Assert.True(File.Exists(zipFile));
+			}
+			finally
+			{
+				if (Directory.Exists(tempDirectory))
+				{
+					Directory.Delete(tempDirectory, recursive: true);
+				}
+		
+				if (File.Exists(tempFile))
+				{
+					File.Delete(tempFile);
+				}
+			}
+		}
+	}
+}
