7dd691f7712479964b1ee539687e3c6a797f82f5
Dave Rostocil <davidr@litmus.com>
2018-06-26 13:17:56 -0400

    Download method

--- LambdaS3FileZipper/Aws/Interfaces/IAwsS3Client.cs
+++ LambdaS3FileZipper/Aws/Interfaces/IAwsS3Client.cs
@@ -7,6 +7,7 @@ namespace LambdaS3FileZipper
     public interface IAwsS3Client
     {
 	    Task<IEnumerable<string>> List(string bucketName, string resource, CancellationToken cancellationToken);
+	    Task Download(string bucketName, string resource, string destinationPath, CancellationToken cancellationToken);
 	    Task Upload(string bucketName, string resourceName, string localFile, CancellationToken cancellationToken);
     }
 }
