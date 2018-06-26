a3afbca11c702287a68fbfdf7d24afa720849cc8
Dave Rostocil <davidr@litmus.com>
2018-06-26 14:56:26 -0400

    implement file zipper

---
+++ LambdaS3FileZipper/FileZipper.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using LambdaS3FileZipper.Interfaces;
+
+namespace LambdaS3FileZipper
+{
+	public class FileZipper : IFileZipper
+	{
+		public async Task<string> Compress(string localDirectory)
+		{
+			if (!Directory.Exists(localDirectory))
+			{
+				throw new IOException($"{localDirectory} does not exist");
+			}
+
+			var directoryName = Path.GetDirectoryName(localDirectory);
+			var zipPath = Path.Combine(localDirectory, $"{directoryName}.zip");
+
+			await Task.Run(() => ZipFile.CreateFromDirectory(localDirectory, zipPath));
+
+			return zipPath;
+		}
+	}
+}
