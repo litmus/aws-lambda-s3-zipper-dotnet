4be0ef4fc18d69ec723569f1d238772ab54ce40b
Dave Rostocil <davidr@litmus.com>
2018-06-27 09:22:53 -0400

    s3 file uploader fixture

---
+++ LambdaS3FileZipper.IntegrationTests/Aws/S3FileUploaderFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using LambdaS3FileZipper.Aws;
+using LambdaS3FileZipper.IntegrationTests.Aws.Interfaces;
+using NUnit.Framework;
+
+namespace LambdaS3FileZipper.IntegrationTests.Aws
+{
+	[TestFixture]
+    public class S3FileUploaderFixture
+	{
+		private readonly S3FileUploader fileUploader;
+		private readonly IS3TestEnvironment testEnvironment;
+
+		public S3FileUploaderFixture()
+		{
+			var s3Client = new AwsS3Client(new AmazonS3Client(RegionEndpoint.USEast1));
+			fileUploader = new S3FileUploader(s3Client);
+			testEnvironment = new EnvironmentVariableS3TestEnvironment();
+		}
+
+	    [Test]
+	    public async Task Upload_ShouldSaveResource()
+	    {
+		    var localTestFile = Path.Combine(Path.GetTempPath(), "uploadTest.txt");
+		    await File.WriteAllTextAsync(localTestFile, "upload test", CancellationToken.None);
+
+		    var result = await fileUploader.Upload(testEnvironment.TestBucket, "uploadTest.txt", localTestFile, CancellationToken.None);
+
+			Assert.NotNull(result);
+		    Assert.True(Uri.TryCreate(result, UriKind.Absolute, out var uri));
+	    }
+    }
+}
