75435ca725f12ecc2362a3e88ec5e9f5f46751ac
Carlos Neninger <carlos@litmus.com>
2018-12-20 15:47:26 -0500

    Fix spacing on ServiceFactory

--- LambdaS3FileZipper/ServiceFactory.cs
+++ LambdaS3FileZipper/ServiceFactory.cs
@@ -3,22 +3,22 @@ using LambdaS3FileZipper.Aws;
 
 namespace LambdaS3FileZipper
 {
-	public class ServiceFactory
-	{
-	    public static Service BuildDefault() => Build(forcePathStyleOnAwsS3: true);
+    public class ServiceFactory
+    {
+        public static Service BuildDefault() => Build(forcePathStyleOnAwsS3: true);
 
         public static Service Build(bool forcePathStyleOnAwsS3)
-	    {
-	        var amazonS3Config = new AmazonS3Config {ForcePathStyle = forcePathStyleOnAwsS3};
-	        var amazonS3Client = new AmazonS3Client(amazonS3Config);
-	        var awsS3Client = new AwsS3Client(amazonS3Client);
+        {
+            var amazonS3Config = new AmazonS3Config {ForcePathStyle = forcePathStyleOnAwsS3};
+            var amazonS3Client = new AmazonS3Client(amazonS3Config);
+            var awsS3Client = new AwsS3Client(amazonS3Client);
 
-	        var retriever = new S3FileRetriever(awsS3Client);
-	        var zipper = new FileZipper();
-	        var uploader = new S3FileUploader(awsS3Client);
+            var retriever = new S3FileRetriever(awsS3Client);
+            var zipper = new FileZipper();
+            var uploader = new S3FileUploader(awsS3Client);
 
-	        var service = new Service(retriever, zipper, uploader);
-	        return service;
+            var service = new Service(retriever, zipper, uploader);
+            return service;
         }
-	}
+    }
 }
