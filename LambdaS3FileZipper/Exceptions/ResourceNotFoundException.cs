075d56d4c32dc39f89d801d315651670363f096d
Carlos Neninger <carlos@litmus.com>
2019-01-24 16:55:26 -0500

    Avoid mutating ResourceNotFoundException message to minimize issues during log serialization

--- LambdaS3FileZipper/Exceptions/ResourceNotFoundException.cs
+++ LambdaS3FileZipper/Exceptions/ResourceNotFoundException.cs
@@ -5,8 +5,15 @@ namespace LambdaS3FileZipper.Exceptions
     public class ResourceNotFoundException : ArgumentException
     {
         public ResourceNotFoundException(string bucket, string resource, string resourceExpressionPattern)
-            : base($"No files found for resource `{bucket}/{resource}` that match expression `{resourceExpressionPattern}`")
+            : base("No files found for given bucket/resource that match the expression")
         {
+            Bucket = bucket;
+            Resource = resource;
+            ResourceExpressionPattern = resourceExpressionPattern;
         }
+
+        public string Bucket { get; set; }
+        public string Resource { get; set; }
+        public string ResourceExpressionPattern { get; set; }
     }
 }
