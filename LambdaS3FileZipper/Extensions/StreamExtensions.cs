effe27b8e736943d44f31ede4a9ac4407a569fc0
Carlos Neninger <carlos@litmus.com>
2020-04-15 17:54:57 -0400

    Add StreamExtensions to ease file content handling in memory

---
+++ LambdaS3FileZipper/Extensions/StreamExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LambdaS3FileZipper.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static async Task<Stream> CopyStreamOntoMemory(this Stream stream, CancellationToken cancellationToken = default)
+        {
+            var memoryStream = new MemoryStream();
+            await stream.CopyToAsync(memoryStream, 4096, cancellationToken).ConfigureAwait(false);
+            return memoryStream.Reset();
+        }
+
+        public static Stream Reset(this Stream stream)
+        {
+            if (stream.CanSeek)
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+            }
+
+            return stream;
+        }
+    }
+}
