900516ec20cc45d29777ff370dce12bdd182f4c2
Carlos Neninger <carlos@litmus.com>
2020-04-20 17:53:43 -0400

    Annotate FileRetriever with documentation on how to use methods RetrieveToLocalDirectory, RetrieveToMemory

--- LambdaS3FileZipper/Interfaces/IFileRetriever.cs
+++ LambdaS3FileZipper/Interfaces/IFileRetriever.cs
@@ -6,7 +6,34 @@ namespace LambdaS3FileZipper.Interfaces
 {
 	public interface IFileRetriever
 	{
-		Task<string> RetrieveToLocalDirectory(string bucket, string resource, string resourceExpressionPattern = default, CancellationToken cancellationToken = default);
-		Task<FileResponse[]> RetrieveToMemory(string bucket, string fileKey, string resourceExpressionPattern = default, CancellationToken cancellationToken = default);
+		/// <summary>
+		/// Retrieves files in given <c><see cref="bucket"/></c> matching name key <c><see cref="resource"/></c> 
+		/// into a local directory named after <c><see cref="bucket"/></c>'s value.
+		///
+		/// <para>Optional: downloaded files can be filtered based on regular expression defined
+		/// by <c><see cref="resourceExpressionPattern"/></c></para>
+		/// </summary>
+		/// <param name="bucket"></param>
+		/// <param name="resource"></param>
+		/// <param name="resourceExpressionPattern">Optional. Should be a valid regular expression</param>
+		/// <param name="cancellationToken">Optional</param>
+		/// <returns>Local directory path</returns>
+		Task<string> RetrieveToLocalDirectory(
+			string bucket, string resource, string resourceExpressionPattern = default, CancellationToken cancellationToken = default);
+
+		/// <summary>
+		/// Retrieves files in given {<see cref="bucket"/>} matching file key {<see cref="fileKey"/>}
+		/// into memory as a collection of <see cref="FileResponse"/>.
+		///
+		/// <para>Optional: downloaded files can be filtered based on regular expression defined
+		/// by <c><see cref="resourceExpressionPattern"/></c></para>
+		/// </summary>
+		/// <param name="bucket"></param>
+		/// <param name="fileKey"></param>
+		/// <param name="resourceExpressionPattern">Optional. Should be a valid regular expression</param>
+		/// <param name="cancellationToken">Optional</param>
+		/// <returns>Collection of files with their content</returns>
+		Task<FileResponse[]> RetrieveToMemory(
+			string bucket, string fileKey, string resourceExpressionPattern = default, CancellationToken cancellationToken = default);
 	}
 }
