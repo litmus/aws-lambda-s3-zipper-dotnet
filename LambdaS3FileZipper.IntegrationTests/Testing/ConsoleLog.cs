169f11f7c0055682bc846be8704dc563c508dfdc
Carlos Neninger <carlos@litmus.com>
2020-04-23 19:11:41 -0400

    Add Serilog package to LambdaS3FileZipper.Test, LambdaS3FileZipper.IntegrationTests to help testing

---
+++ LambdaS3FileZipper.IntegrationTests/Testing/ConsoleLog.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace LambdaS3FileZipper.IntegrationTests.Testing
+{
+	public static class ConsoleLog
+	{
+		/// <summary>
+		/// Enables Serilog logging to the Console
+		/// </summary>
+		public static void EnableSerilog()
+		{
+			Log.Logger = new LoggerConfiguration().MinimumLevel.Verbose().WriteTo.Console().CreateLogger();
+		}
+	}
+}
